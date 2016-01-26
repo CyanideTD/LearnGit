b6b90f002112fb1f7089df89118c5bd19164f7cb
老爸 <QihangChen@host-studentw-141-104.dhcp.stevens-tech.edu>
2016-01-26 16:46:21 -0500

    Initial Commit

---
+++ hello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by 启航 陈 on 1/26/16.
+//  Copyright © 2016 启航 陈. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
